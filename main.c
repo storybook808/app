3ac954badd640d893a9fb6d19dc5686bd29a5cc8
Steven Chen <scbccb100@gmail.com>
2015-03-22 12:13:55 -1000

    Checking Ideal Values

--- main.c
+++ main.c
@@ -24,25 +24,61 @@ void main(void) {
 	HAL_Init();
 	SystemClock_Config();
 	initSystem();
-	calibrateWall(FRONT);
+
+
+//	while(HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13) != GPIO_PIN_SET);
 
 	//LED start up sequence
 	testChaser(1, 250);
 
-	 // Test USART Connection
-	 printString("Hello world!");
-	 printNL();
+	calibrateSensors();
 
-	 setVelocity(500);
-	 HAL_Delay(300);
+	// Test USART Connection
+	printString("Hello world!");
+	printNL();
 
-	 setLeftVelocity(500);
+	double left_front_sensor, right_front_sensor;
+	double left_side_sensor, right_side_sensor;
 
 	while(1) {
 		batteryFault();
-		 brakeRight();
-		 HAL_Delay(5);
 
-		 setRightVelocity(30);
+		left_front_sensor = readSensor(LEFT_DET);
+		right_front_sensor = readSensor(RIGHT_DET);
+
+		left_side_sensor = readSensor(LEFT_CEN_DET);
+		right_side_sensor = readADC(RIGHT_CEN_DET);
+
+		if (left_front_sensor <= getWall(IDEALLEFTFRONT)) {
+			setLED(WHITE);
+		}
+
+		else {
+			resetLED(WHITE);
+		}
+
+		if (right_front_sensor <= getWall(IDEALRIGHTFRONT)) {
+			setLED(BLUE);
+		}
+
+		else {
+			resetLED(BLUE);
+		}
+
+		if (left_side_sensor <= getWall(IDEALLEFTCENTER)) {
+			setLED(GREEN);
+		}
+
+		else {
+			resetLED(GREEN);
+		}
+
+		if (right_side_sensor <= getWall (IDEALRIGHTCENTER)) {
+			setLED(RED);
+		}
+
+		else {
+			resetLED(RED);
+		}
 	}
 }
