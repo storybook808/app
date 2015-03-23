cb52c1da5b52c930ea1ae506077e146f1e09356c
Steven Chen <scbccb100@gmail.com>
2015-03-22 19:38:58 -1000

    Rough Right Wall Follower

--- main.c
+++ main.c
@@ -19,14 +19,12 @@
 #include "usart.h"
 #include "system.h"
 
+extern TIM_HandleTypeDef htim2;
 
 void main(void) {
 	HAL_Init();
 	SystemClock_Config();
 	initSystem();
-
-	//LED start up sequence
-	testChaser(1, 250);
 
 	calibrateSensors();
 
@@ -34,8 +32,33 @@ void main(void) {
 	printString("Hello world!");
 	printNL();
 
+	while(!getButton()) {
+		toggleLED(WHITE);
+	}
+
+	resetLED(WHITE);
+
+	testChaser(1,250);
+
 	double left_front_sensor, right_front_sensor;
 	double left_side_sensor, right_side_sensor;
+
+	double error;
+
+	const double kP = 1;
+
+	double base_speed = 50;
+
+	int start_encoder;
+
+	const int forward_distance = 3500;
+
+	const int left_turn = 1200;
+	const int right_turn = 1150;
+
+//	setVelocity(0);
+	setLeftVelocity(0);
+	setRightVelocity(0);
 
 	while(1) {
 		batteryFault();
@@ -46,37 +69,87 @@ void main(void) {
 		left_side_sensor = readSensor(LEFT_CEN_DET);
 		right_side_sensor = readSensor(RIGHT_CEN_DET);
 
+		resetLEDAll();
 
-		if (left_front_sensor <= getWall(IDEALLEFTFRONT)) {
-			setLED(WHITE);
-		}
-
-		else {
-			resetLED(WHITE);
-		}
-
-		if (right_front_sensor <= getWall(IDEALRIGHTFRONT)) {
+		if (right_side_sensor >= getWall(FARRIGHTWALL)) {
+			// Move forward, brake, turn right, and move forward
 			setLED(BLUE);
+//			setVelocity(base_speed);
+			setSpeed(LEFTMOTOR, base_speed);
+			setSpeed(RIGHTMOTOR, base_speed);
+			start_encoder = getEncoder(RIGHTENCODER);
+			while (getEncoder(RIGHTENCODER) < (start_encoder + forward_distance));
+			setSpeed(LEFTMOTOR, 0);
+			setSpeed(RIGHTMOTOR, 0);
+			HAL_TIM_Base_Start_IT(&htim2);
+//			brakeLeft();
+//			brakeRight();
+			HAL_Delay(500);
+			HAL_TIM_Base_Stop_IT(&htim2);
+			setSpeed(LEFTMOTOR, 200);
+			setSpeed(RIGHTMOTOR, -200);
+//			setLeftVelocity(base_speed);
+//			setRightVelocity(-base_speed);
+			start_encoder = getEncoder(LEFTENCODER);
+			while (getEncoder(LEFTENCODER) < (start_encoder + right_turn));
+			setSpeed(LEFTMOTOR, 0);
+			setSpeed(RIGHTMOTOR, 0);
+			HAL_TIM_Base_Start_IT(&htim2);
+//			brakeLeft();
+//			brakeRight();
+			HAL_Delay(500);
+			HAL_TIM_Base_Stop_IT(&htim2);
+//			while(1) {
+//				batteryFault();
+//				if (getButton()) break;
+//			}
 		}
+
 
 		else {
-			resetLED(BLUE);
+
+			if (left_front_sensor <= getWall(IDEALLEFTFRONT) && right_front_sensor <= getWall(IDEALRIGHTFRONT)) {
+				// Brake & turn left
+				setLED(WHITE);
+//				brakeLeft();
+//				brakeRight();
+				setSpeed(LEFTMOTOR, 0);
+				setSpeed(RIGHTMOTOR, 0);
+				HAL_TIM_Base_Start_IT(&htim2);
+				HAL_Delay(500);
+				HAL_TIM_Base_Stop_IT(&htim2);
+				setSpeed(LEFTMOTOR, -200);
+				setSpeed(RIGHTMOTOR, 200);
+//				setLeftVelocity(-base_speed);
+//				setRightVelocity(base_speed);
+				start_encoder = getEncoder(RIGHTENCODER);
+				while (getEncoder(RIGHTENCODER) < (start_encoder + left_turn));
+//				brakeLeft();
+//				brakeRight();
+				setSpeed(LEFTMOTOR, 0);
+				setSpeed(RIGHTMOTOR, 0);
+				HAL_TIM_Base_Start_IT(&htim2);
+				HAL_Delay(500);
+				HAL_TIM_Base_Stop_IT(&htim2);
+//				while(1) {
+//					batteryFault();
+//					if (getButton()) break;
+//				}
+			}
+
+			else {
+				// Move forward correcting off the right wall
+				error = getWall(IDEALRIGHTCENTER) - right_side_sensor;
+
+				setSpeed(LEFTMOTOR, (int)(base_speed - (error * kP)));
+				setSpeed(RIGHTMOTOR, (int)(base_speed + (error * kP)));
+//				setLeftVelocity(base_speed - (error * kP));
+//				setRightVelocity(base_speed + (error * kP));
+
+			}
 		}
 
-		if (left_side_sensor <= getWall(IDEALLEFTCENTER)) {
-			setLED(GREEN);
-		}
 
-		else {
-			resetLED(GREEN);
-		}
 
-		if (right_side_sensor <= getWall(IDEALRIGHTCENTER)) {
-			setLED(RED);
-		}
-
-		else {
-			resetLED(RED);
-		}
 	}
 }
